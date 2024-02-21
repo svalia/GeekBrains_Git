edf60828f3f79740091aec59e13327fb43a75c0a
Valia Smolin <Valia@Smolin.Ru>
2024-02-22 00:48:10 +0300

    Rename folder

---
+++ Introduction to programming languages/lesson2_Arrays/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Создать массив и заполнить его целыми числами от 1 до N 
+// (N - произвольное натуральное число). 
+// Вывести элементы массива на экран.
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+// n = n + 1;
+int[] arr = new int[n];
+for (int i = 0; i < n; i++)
+//for (int i = 1; i < arr.Length; i++) //Альтернативыный вариант arr.length - где .length - функция длинны масисва arr
+{
+	arr[i] = i;
+	Console.WriteLine($"{arr[i]} ");
+	// Console.WriteLine(" ");
+}
+
+  
---
+++ Introduction to programming languages/lesson2_Arrays/task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано N гирь с разным весом в случайном порядке. Определить вес самой тяжелой гири.
+int n = 5;
+int[] array = { 2, 4, 7, 3, 9 };
+int i = 0;
+int max = array[0];
+
+while (i < n)
+{
+	if (array[i] > max)
+	{
+		max = array[i];
+	}
+	i = i + 1;
+}
+Console.WriteLine(max);
