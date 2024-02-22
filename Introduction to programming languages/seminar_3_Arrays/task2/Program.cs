6899a8e0990117969ce41d4909c991f26f607416
Valia Smolin <Valia@Smolin.Ru>
2024-02-22 23:31:19 +0300

    Add task2

--- Introduction to programming languages/seminar_3_Arrays/task2/Program.cs
+++ Introduction to programming languages/seminar_3_Arrays/task2/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив из 10 элементов, заполненный числами изпромежутка [-10, 10]. 
+// Замените отрицательные элементы наположительные, а положительные на отрицательные.
+// Пример: [1 -5 6]=> [-1 5 -6]
+void Main()
+{
+	int[] array = GenerateArray(10, -10, 10);
+	System.Console.WriteLine("Сгенерированный массив: ");
+	PrintArray(array);
+	ChangeArray(array);
+	System.Console.WriteLine("");
+	System.Console.WriteLine("Измененный массив: ");
+	PrintArray(array);
+}
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+	int[] myArray = new int[size];
+	for (int i = 0; i < myArray.Length; i++)
+	{
+		myArray[i] = new Random().Next(leftRange, rightRange + 1);
+	}
+	return myArray;
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+	for (int i = 0; i < arrayForPrint.Length; i++)
+	{
+		System.Console.Write(arrayForPrint[i] + " ");
+	}
+}
+
+void ChangeArray(int[] myArray)
+{
+	for (int i = 0; i < myArray.Length; i++)
+	{
+		myArray[i] = -myArray[i];
+	}
+}
+Main();
