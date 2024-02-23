d1f0348e634bb328880b2f71afc5dde71e0de96d
Valia Smolin <Valia@Smolin.Ru>
2024-02-24 01:11:57 +0300

    Lesson 4 Add task3

---
+++ Introduction to programming languages/lesson4_2xArrays/task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Считать с консоли строку, состоящую изцифр и латинских букв.
+// Сформировать новую строку, состоящую избукв исходной строки.
+void Main()
+{
+string str = Console.ReadLine();
+string result = GetLettersFromStr(str);
+Console.WriteLine(result);
+}
+
+string GetLettersFromStr(string s)
+{
+string letters = "";
+foreach (char e in s)
+{
+	if (char.IsAsciiLetter(e) == true)
+	{
+		letters = letters + e;
+	}
+}
+return letters;
+}
+
+Main();
