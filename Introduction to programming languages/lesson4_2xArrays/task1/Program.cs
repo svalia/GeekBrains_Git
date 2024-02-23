9e50a2ba4cc0e05af253a040d94272dd5e4198be
Valia Smolin <Valia@Smolin.Ru>
2024-02-23 23:26:08 +0300

    lesson4 add task1

---
+++ Introduction to programming languages/lesson4_2xArrays/task1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+void Main()
+{
+	int[,] matrix = CreateMatrix(4, 5);
+	ShowMatrix(matrix);
+}
+int[,] CreateMatrix(int rowCount, int columnCount)
+{
+	int[,] matrix = new int[rowCount, columnCount];
+
+	Random rnd = new Random();
+	for (int i = 0; i < matrix.GetLength(0); i++)// array.GetLength(0) - узнаем сколько строк в двумерном массиве, где 0 означает что мы запрашиваем кол-во строк (т.е. одномерных массивов)
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)// // array.GetLength(1) - узнаем сколько элементов внутри каждого одномерного массива, находящихся в двумерном массиве, где 1 указывает как раз на кол-во столбцов (элементов внутри одномерных массивов) двумерного массива
+		{
+			matrix[i, j] = rnd.Next(1, 11);
+		}
+	}
+	return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)
+		{
+			Console.Write($"{matrix[i, j]} "); //интерполяция строк
+		}
+		Console.WriteLine();
+	}
+}
+Main();
