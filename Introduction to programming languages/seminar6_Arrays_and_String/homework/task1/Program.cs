d60c9a51d9003ce5f7237724da5cf30a5526aa2f
Valia Smolin <Valia@Smolin.Ru>
2024-02-27 00:55:46 +0300

    Add task 1 in seminar6

--- Introduction to programming languages/seminar6_Arrays_and_String/homework/task1/Program.cs
+++ Introduction to programming languages/seminar6_Arrays_and_String/homework/task1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Создание и инициализация двумерного массива символов
+        char[,] charArray = {
+            { 'H', 'e', 'l', 'l', 'o' },
+            { 'W', 'o', 'r', 'l', 'd' }
+        };
+
+        // Создание строки для хранения результатов
+        string result = "";
+
+        // Получение размеров массива
+        int rows = charArray.GetLength(0); // Количество строк
+        int cols = charArray.GetLength(1); // Количество столбцов
+
+        // Перебор всех элементов двумерного массива
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                // Добавление текущего символа в строку
+                result += charArray[i, j];
+            }
+        }
+
+        // Вывод результата
+        Console.WriteLine(result);
+    }
+}
