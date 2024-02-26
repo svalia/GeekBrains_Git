499c3f8195407a1e2b428e58679edc697b254ba7
Valia Smolin <Valia@Smolin.Ru>
2024-02-27 02:03:22 +0300

    Add task 3 in seminar7 Certification

---
+++ Introduction to programming languages/seminar7_Recursion/Certification/task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3: Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+void Main()
+{
+	int[] array = { 1, 2, 3, 4, 5 }; // Пример массива
+	PrintArrayReverse(array, array.Length - 1);
+}
+
+// Метод для рекурсивной печати элементов массива с конца
+void PrintArrayReverse(int[] array, int index)
+{
+	if (index < 0) // Базовый случай рекурсии
+	{
+		return;
+	}
+
+	Console.WriteLine(array[index]); // Печать текущего элемента
+	PrintArrayReverse(array, index - 1); // Рекурсивный вызов для следующего элемента
+}
+Main();
