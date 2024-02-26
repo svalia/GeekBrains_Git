323a84f77908a0fab615a24ab621ec894bfd7aa7
Valia Smolin <Valia@Smolin.Ru>
2024-02-27 01:47:36 +0300

    Add task 1 in seminar7 Certification

---
+++ Introduction to programming languages/seminar7_Recursion/Certification/task1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы.
+
+void Main()
+    {
+        int M = ReadAndConvert("Введите значение M: ");
+        int N = ReadAndConvert("Введите значение N: ");
+
+        Console.WriteLine($"Натуральные числа от {M} до {N}:");
+        PrintNumbers(M, N);
+    }
+
+  // int ReadAndConvert(string message)
+  //   {
+  //       Console.Write(message);
+  //       return int.Parse(Console.ReadLine());
+  //   }
+	
+	int ReadAndConvert(string message)
+{
+    Console.Write(message);
+    string input = Console.ReadLine() ?? "0"; // Если введено null, используем "0" как значение по умолчанию
+    return int.Parse(input);
+}
+
+  void PrintNumbers(int M, int N)
+    {
+        if (M > N)
+        {
+            return; // Если M больше N, рекурсия прекращается.
+        }
+        Console.WriteLine(M);
+        PrintNumbers(M + 1, N); // Рекурсивный вызов функции с увеличенным M.
+    }
+
+		Main();
+
